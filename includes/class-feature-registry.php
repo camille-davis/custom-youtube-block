df70caca3c51e6d0b2fff68c55de50fe51238980
Camille Davis <camilledavisdev@gmail.com>
2025-12-25 20:38:01 -0800

    Make help text more concise.

--- includes/class-feature-registry.php
+++ includes/class-feature-registry.php
@@ -27,32 +27,32 @@ class Custom_YouTube_Block_Feature_Registry {
 			'fullwidth' => array(
 				'slug' => 'fullwidth',
 				'label' => __( 'Fullwidth', 'custom-youtube-block' ),
-				'help' => __( 'Make youtube video fullwidth.', 'custom-youtube-block' ),
+				'help' => __( '', 'custom-youtube-block' ),
 			),
 			'autoplay' => array(
 				'slug' => 'autoplay',
 				'label' => __( 'Autoplay', 'custom-youtube-block' ),
-				'help' => __( 'Automatically play the video when the page loads. Video will be muted.', 'custom-youtube-block' ),
+				'help' => __( 'Note: Video will be muted.', 'custom-youtube-block' ),
 			),
 			'loop' => array(
 				'slug' => 'loop',
 				'label' => __( 'Loop Video', 'custom-youtube-block' ),
-				'help' => __( 'Make the video loop continuously when it reaches the end.', 'custom-youtube-block' ),
+				'help' => __( '', 'custom-youtube-block' ),
 			),
 			'hideControls' => array(
 				'slug' => 'hide-controls',
 				'label' => __( 'Hide Controls', 'custom-youtube-block' ),
-				'help' => __( 'Remove video player controls from the YouTube embed.', 'custom-youtube-block' ),
+				'help' => __( '', 'custom-youtube-block' ),
 			),
 			'hideRelatedVideos' => array(
 				'slug' => 'hide-related-videos',
 				'label' => __( 'Hide Related Videos', 'custom-youtube-block' ),
-				'help' => __( 'Hide related videos that appear at the end of the video. Only videos from the same channel will be shown.', 'custom-youtube-block' ),
+				'help' => __( 'Hide related video thumbnails at the end of the video. (Related videos may still appear on pause.)', 'custom-youtube-block' ),
 			),
 			'disableInteraction' => array(
 				'slug' => 'disable-interaction',
 				'label' => __( 'Disable Interaction', 'custom-youtube-block' ),
-				'help' => __( 'Prevent all mouse and keyboard interactions with the video player, including pause on click and keyboard controls. Warning: This blocks all user interactions.', 'custom-youtube-block' ),
+				'help' => __( 'Prevent all mouse and keyboard interactions with the video player.', 'custom-youtube-block' ),
 			),
 		);
 	}
